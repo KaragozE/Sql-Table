9e1b99750b48ab4c9d49bc5d531f575e1b1ed3f4
KaragozE <karagoz.eray@gmail.com>
2018-11-27 11:51:08 +0300

    first commit

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string connetionString = "Data Source=DMGM0349997\\SQLEXPRESS;Initial Catalog=eray;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connetionString);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string q = "insert into Table_4(ID,Name)values('" + txtID.Text.ToString() + "','" + txtName.Text.ToString() + "')";
+                SqlCommand cmd = new SqlCommand(q, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Connection made successfully..!");
+                    
+            }
+        }
+    }
+}
